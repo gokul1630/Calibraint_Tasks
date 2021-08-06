d36064882eeac159e8379440d399b3a4a0fd2ae8
gokul1630 <gokulgp163034@gmail.com>
2021-08-06 16:52:24 +0530

    Show Navbar only After loggedin

--- day_5/src/App.js
+++ day_5/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import AddTodo from './Components/AddTodo';
+import Login from './Components/Login';
 import NavBar from './Components/NavBar';
+import SignUp from './Components/Signup';
 import Todos from './Components/Todos';
+import Loader from './Components/Loader';
 import { store } from './redux/store';
 
 function App(props) {
+  const NavRoute = ({ exact, path, component: Component }) => (
+    <Route
+      exact={exact}
+      path={path}
+      render={(props) => (
+        <div>
+          <NavBar />
+          <Component {...props} />
+        </div>
+      )}
+    />
+  );
+
   return (
     <HashRouter basename="/">
       <Provider store={store}>
+        <Switch>
           <Route exact path="/" component={Loader}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/signup" component={SignUp}></Route>
+          <NavRoute exact path="/todos" component={Todos}></NavRoute>
+          <NavRoute exact path="/addTodo" component={AddTodo}></NavRoute>
+        </Switch>
       </Provider>
     </HashRouter>
   );
